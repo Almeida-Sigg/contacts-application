da5aa29f380a9740d7307160e079c9a2a92da1c8
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-28 23:58:08 -0500

    update

--- src/ContactPractice.java
+++ src/ContactPractice.java
@@ -1,4 +1,21 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ContactPractice {
+    String directory = "data";
+    String filename = "contact.txt";
+
+    Path dataDirectory = Paths.get(directory);
+    Path dataFile = Paths.get(directory, filename);
+
+//    if (Files.notExists(dataDirectory)) {
+//        Files.createDirectories(dataDirectory);
+//    }
+//
+//    if (! Files.exists(dataFile)) {
+//            Files.createFile(dataFile);
+//    }
     public static void showMenu(){
         System.out.println( "1. View contacts.\n" +
                 "2. Add a new contact.\n" +
@@ -7,6 +24,9 @@ public class ContactPractice {
                 "5. Exit.\n" +
                 "Enter an option (1, 2, 3, 4 or 5):");
     }
+
+
+
 
 
     public static void main(String[] args) {
